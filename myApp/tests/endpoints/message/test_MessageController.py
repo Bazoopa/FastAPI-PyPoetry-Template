64d14e87666173c66cc21c01851289fa9807c842
BruceMcD <162571835+BruceMcD@users.noreply.github.com>
2024-07-09 17:22:16 +0100

    Adding tests

---
+++ myApp/tests/endpoints/message/test_MessageController.py
@@ -0,0 +1,14 @@
+def test_get_message():
+    assert False
+
+
+def test_get_messages():
+    assert False
+
+
+def test_create_message():
+    assert False
+
+
+def test_delete_message():
+    assert False
